72ee3ff1f4195e995c76517e855c81a832f64152
dscifo <diego.scifo@globallogic.com>
2023-05-23 09:18:36 -0300

    Random color. Connected user.

--- spring-boot-backend-chat/src/main/java/com/dss/springboot/backend/chat/controller/ChatController.java
+++ spring-boot-backend-chat/src/main/java/com/dss/springboot/backend/chat/controller/ChatController.java
@@ -6,15 +6,31 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.util.Date;
+import java.util.Random;
 
 @Controller
 public class ChatController {
+
+    private String[] colors = {"red", "green", "blue", "magenta", "purple", "orange"};
+
     @MessageMapping("/message")
     @SendTo("/chat/message")
     public Message receiveMessage(Message message) {
-
         message.setDate(new Date().getTime());
-        message.setText("Received by Broker: " + message.getText());
+
+        if (message.getType().equals("NEW_USER")) {
+            message.setText("Nuevo usuario");
+            int random = new Random().nextInt(colors.length);
+            message.setColor(colors[random]);
+        }
+
         return message;
+
+    }
+
+    @MessageMapping("/writing")
+    @SendTo("/chat/writing")
+    public String isWriting(String username) {
+        return username.concat(" está escribiendo...");
     }
 }
--- spring-boot-backend-chat/src/main/java/com/dss/springboot/backend/chat/domain/document/Message.java
+++ spring-boot-backend-chat/src/main/java/com/dss/springboot/backend/chat/domain/document/Message.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 public class Message implements Serializable {
     private String text;
     private Long date;
+    private String username;
+    private String type;
+    private String color;
 
     public String getText() {
         return text;
@@ -20,5 +23,29 @@ public class Message implements Serializable {
 
     public void setDate(Long date) {
         this.date = date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
