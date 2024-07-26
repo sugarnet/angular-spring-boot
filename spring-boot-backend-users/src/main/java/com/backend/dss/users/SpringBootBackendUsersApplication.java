d8b371f8453c769ec46e8b0749ac12f766018f11
dscifo <diego.scifo@globallogic.com>
2024-07-26 18:35:22 -0300

    create users project

---
+++ spring-boot-backend-users/src/main/java/com/backend/dss/users/SpringBootBackendUsersApplication.java
@@ -0,0 +1,13 @@
+package com.backend.dss.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendUsersApplication.class, args);
+	}
+
+}
